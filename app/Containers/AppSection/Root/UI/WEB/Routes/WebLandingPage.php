b4aedec011668e146c363abd7f632078e34e70dd
David Rey <neisserrey@upeu.edu.pe>
2024-01-31 18:58:32 -0500

    fix: :adhesive_bandage: Eliminando todas las demas paginas .blade
    Nos qeudamos solo con la SPA en ROOT

--- app/Containers/AppSection/Root/UI/WEB/Routes/WebLandingPage.php
+++ app/Containers/AppSection/Root/UI/WEB/Routes/WebLandingPage.php
@@ -7,8 +7,8 @@ $webDomain = env('APP_URL', 'http://apiato.dev.com');
 Route::get('/', [Controller::class, 'sayWelcome'])
     ->name('web_root');
 
-Route::group(['domain' => parse_url($webDomain, PHP_URL_HOST)], function () {
-    // Handle for vue router only in web domain
-    Route::get('/{any}', [Controller::class, 'sayWelcome'])
-        ->where('any', '^(?!api(?:\/|$)).*');
-});
+// Route::group(['domain' => parse_url($webDomain, PHP_URL_HOST)], function () {
+// Handle for vue router only in web domain
+Route::get('/{any}', [Controller::class, 'sayWelcome'])
+    ->where('any', '^(?!api(?:\/|$)).*');
+// });
