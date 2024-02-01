6c0ecec94272c0fba3f8bf2690fa67bc86aa059d
David Rey <neisserrey@upeu.edu.pe>
2024-01-31 19:56:18 -0500

    fix: :bug: Mejorando la respuesta del profile.

--- app/Containers/AppSection/Authentication/UI/API/Controllers/GetAuthenticatedUserController.php
+++ app/Containers/AppSection/Authentication/UI/API/Controllers/GetAuthenticatedUserController.php
@@ -3,6 +3,7 @@
 namespace App\Containers\AppSection\Authentication\UI\API\Controllers;
 
 use App\Containers\AppSection\Authentication\Actions\GetAuthenticatedUserAction;
+use App\Containers\AppSection\Authorization\UI\API\Transformers\RoleTransformer;
 use App\Containers\AppSection\User\UI\API\Transformers\UserTransformer;
 use App\Ship\Parents\Controllers\ApiController;
 
@@ -13,10 +14,19 @@ class GetAuthenticatedUserController extends ApiController
     ) {
     }
 
-    public function __invoke(): array
+    public function __invoke()
     {
         $user = $this->getAuthenticatedUserAction->run();
 
-        return $this->transform($user, UserTransformer::class);
+        return $this->json([
+            'data' => [
+                'user' => $this->transform($user, UserTransformer::class),
+                // 'person' => $this->transform($user->person, PersonTransformer::class),
+                // 'privileges' => $this->transform($user->privileges, PrivilegeTransformer::class),
+                // 'modulos' => $this->transform($user->modulos, ModuloTransformer::class),
+                // 'permissions' => $this->transform($permissions, PermissionTransformer::class),
+                'roles' => $this->transform($user->roles, RoleTransformer::class),
+            ]
+        ]);
     }
 }
