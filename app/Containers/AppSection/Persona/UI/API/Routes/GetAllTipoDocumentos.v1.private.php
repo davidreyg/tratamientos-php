060c7bdb933784d6dc6998e3e14f27369086a2ce
David Rey <neisserrey@upeu.edu.pe>
2024-02-01 17:47:26 -0500

    feat: :sparkles: Agregamos endpoint para devolver los tipos de documento

---
+++ app/Containers/AppSection/Persona/UI/API/Routes/GetAllTipoDocumentos.v1.private.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @apiGroup           Persona
+ * @apiName            GetAllTipoDocumentos
+ *
+ * @api                {GET} /v1/tipo-documentos Get All TipoDocumentos
+ * @apiDescription     Endpoint description here...
+ *
+ * @apiVersion         1.0.0
+ * @apiPermission      Authenticated ['permissions' => '', 'roles' => '']
+ *
+ * @apiHeader          {String} accept=application/json
+ * @apiHeader          {String} authorization=Bearer
+ *
+ * @apiParam           {String} parameters here...
+ *
+ * @apiSuccessExample  {json} Success-Response:
+ * HTTP/1.1 200 OK
+ * {
+ *     // Insert the response of the request here...
+ * }
+ */
+
+use App\Containers\AppSection\Persona\UI\API\Controllers\TipoDocumentoController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('tipo-documentos', [TipoDocumentoController::class, 'getALl'])
+    ->middleware(['auth:api']);
+
