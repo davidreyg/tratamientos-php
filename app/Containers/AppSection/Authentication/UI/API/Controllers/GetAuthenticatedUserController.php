a80cabee7dd9197d3e7bf6e060f036def00b8235
David Rey <neisserrey@upeu.edu.pe>
2024-02-01 10:32:18 -0500

    feat: :sparkles: Devolviendo el establecimiento al solicitar profile

--- app/Containers/AppSection/Authentication/UI/API/Controllers/GetAuthenticatedUserController.php
+++ app/Containers/AppSection/Authentication/UI/API/Controllers/GetAuthenticatedUserController.php
@@ -4,6 +4,7 @@ namespace App\Containers\AppSection\Authentication\UI\API\Controllers;
 
 use App\Containers\AppSection\Authentication\Actions\GetAuthenticatedUserAction;
 use App\Containers\AppSection\Authorization\UI\API\Transformers\RoleTransformer;
+use App\Containers\AppSection\Establecimiento\UI\API\Transformers\EstablecimientoTransformer;
 use App\Containers\AppSection\User\UI\API\Transformers\UserTransformer;
 use App\Ship\Parents\Controllers\ApiController;
 
@@ -24,7 +25,7 @@ class GetAuthenticatedUserController extends ApiController
                 // 'person' => $this->transform($user->person, PersonTransformer::class),
                 // 'privileges' => $this->transform($user->privileges, PrivilegeTransformer::class),
                 // 'modulos' => $this->transform($user->modulos, ModuloTransformer::class),
-                // 'permissions' => $this->transform($permissions, PermissionTransformer::class),
+                'establecimiento' => $this->transform($user->establecimiento, EstablecimientoTransformer::class),
                 'roles' => $this->transform($user->roles, RoleTransformer::class),
             ]
         ]);
