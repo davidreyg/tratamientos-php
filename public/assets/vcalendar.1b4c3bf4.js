355ad93d107f16d0ed6f59fed17b6dd6a10ebac1
David Rey <neisserrey@upeu.edu.pe>
2024-02-05 17:10:17 -0500

    feat: :bookmark: v0.4
    http://sgecdls.dirislimasur.gob.pe/ dominio

---
+++ public/assets/vcalendar.1b4c3bf4.js
@@ -0,0 +1,1 @@
+import{b as s}from"./index.7c005f61.js";import{s as o}from"./index.5b54f067.js";var e=s(async({app:a})=>{a.use(o,{})});export{e as default};
--- public/assets/vue-tostification.20f55e1d.js
+++
@@ -1,1 +0,0 @@
-import{b as t,e as o}from"./index.8cb823b6.js";var s=t(async({app:a})=>{a.use(o,{transition:"Vue-Toastification__bounce",maxToasts:20,newestOnTop:!0})});export{s as default};
