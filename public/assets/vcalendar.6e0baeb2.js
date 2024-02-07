9d359dd843befe4300d421b0bf54aded5e110e58
David Rey <neisserrey@upeu.edu.pe>
2024-02-06 21:55:46 -0500

    feat: :ambulance: ?? que paso con localhost

--- public/assets/vcalendar.6e0baeb2.js
+++
@@ -1,1 +0,0 @@
-import{b as s,d as e}from"./index.0c983d3a.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
--- public/assets/vue-tostification.4c8adad1.js
+++
@@ -1,1 +0,0 @@
-import{b as t,c as o}from"./index.0c983d3a.js";var e=t(async({app:a})=>{a.use(o,{transition:"Vue-Toastification__bounce",maxToasts:20,newestOnTop:!0})});export{e as default};
