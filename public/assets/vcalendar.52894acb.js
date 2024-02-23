737640d777cef1e2154f81540d346b792abaeb14
David Rey <neisserrey@upeu.edu.pe>
2024-02-22 23:59:19 -0500

    feat: :ambulance: EL formulario prinicpal a un 50%
    aun no guarda el control

--- public/assets/vcalendar.52894acb.js
+++
@@ -1,1 +0,0 @@
-import{b as s,d as e}from"./index.39fb80a1.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
---
+++ public/assets/vcalendar.806cd8d9.js
@@ -0,0 +1,1 @@
+import{b as s,d as e}from"./index.dc07db66.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
