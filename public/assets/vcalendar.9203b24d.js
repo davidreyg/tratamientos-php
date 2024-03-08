296be0d1e62857b9f86b4d8f4c2d0503db49e043
David Rey <neisserrey@upeu.edu.pe>
2024-03-08 12:18:11 -0500

    fix: :bug: parece que ahora si? xdxd

---
+++ public/assets/vcalendar.9203b24d.js
@@ -0,0 +1,1 @@
+import{b as s,d as e}from"./index.cb0e24bc.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
---
+++ public/assets/vue-tostification.48a0a938.js
@@ -0,0 +1,1 @@
+import{b as t,c as o}from"./index.cb0e24bc.js";var e=t(async({app:a})=>{a.use(o,{transition:"Vue-Toastification__bounce",maxToasts:20,newestOnTop:!0})});export{e as default};
