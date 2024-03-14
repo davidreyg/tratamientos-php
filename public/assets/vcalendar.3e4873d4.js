d8be8153e0e7ead3c67d904f83867856710ef47a
David Rey <neisserrey@upeu.edu.pe>
2024-03-14 13:35:04 -0500

    chore: :rocket: Probamos la nueva version de resultados

---
+++ public/assets/vcalendar.3e4873d4.js
@@ -0,0 +1,1 @@
+import{b as s,d as e}from"./index.4de37196.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
---
+++ public/assets/vue-tostification.477f00d2.js
@@ -0,0 +1,1 @@
+import{b as t,c as o}from"./index.4de37196.js";var e=t(async({app:a})=>{a.use(o,{transition:"Vue-Toastification__bounce",maxToasts:20,newestOnTop:!0})});export{e as default};
