85eca0a077e6eb758518555c0e8755ecfd565c08
David Rey <neisserrey@upeu.edu.pe>
2024-03-14 21:17:55 -0500

    feat: :rocket: primera version de registro de resultados

---
+++ public/assets/vcalendar.2d2e1e08.js
@@ -0,0 +1,1 @@
+import{b as s,d as e}from"./index.f6335575.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
--- public/assets/vcalendar.3e4873d4.js
+++
@@ -1,1 +0,0 @@
-import{b as s,d as e}from"./index.4de37196.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
