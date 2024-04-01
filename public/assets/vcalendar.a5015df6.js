a3685a2bddb1912e2987d7d411c9e6c1c8688bdc
David Rey <neisserrey@upeu.edu.pe>
2024-03-31 19:33:10 -0500

    feat: :sparkles: excel frontend

---
+++ public/assets/vcalendar.a5015df6.js
@@ -0,0 +1,1 @@
+import{b as s,d as e}from"./index.418160ac.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
--- public/assets/vue-tostification.f2e22fc0.js
+++
@@ -1,1 +0,0 @@
-import{b as t,c as o}from"./index.7d3e52a8.js";var e=t(async({app:a})=>{a.use(o,{transition:"Vue-Toastification__bounce",maxToasts:20,newestOnTop:!0})});export{e as default};
