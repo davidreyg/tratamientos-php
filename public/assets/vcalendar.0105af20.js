1ddfa5034a631d762e470591b30180b78b3c5a47
David Rey <neisserrey@upeu.edu.pe>
2024-04-07 15:27:26 -0500

    fix: :ambulance: cambiamos un error de nulo en el frontend

---
+++ public/assets/vcalendar.0105af20.js
@@ -0,0 +1,1 @@
+import{b as s,d as e}from"./index.1470aaf9.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
--- public/assets/vcalendar.090e7536.js
+++
@@ -1,1 +0,0 @@
-import{b as s,d as e}from"./index.d6593c0d.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
