d1ddaa0cc78344cdf5d6c625eca132860bef6194
David Rey <neisserrey@upeu.edu.pe>
2024-04-28 15:06:19 -0500

    feat: :lipstick: CRUD EXAMENES 1.0 OK

--- public/assets/IndexPage.455f3459.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.cd754ea2.js";import{_ as t,e as a,w as r,o as s,f as c}from"./index.aead979f.js";const o={};function n(f,_){return s(),a(e,{class:"row items-center justify-evenly"},{default:r(()=>[c(" index ")]),_:1})}var x=t(o,[["render",n]]);export{x as default};
---
+++ public/assets/vcalendar.df3a6e9a.js
@@ -0,0 +1,1 @@
+import{b as s,d as e}from"./index.a85a2474.js";var o=s(async({app:a})=>{a.use(e,{})});export{o as default};
